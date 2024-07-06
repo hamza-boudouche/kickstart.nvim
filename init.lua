3ffb3edf4a1b9834e9dbd941f19737fe72a81e7b
hamza-boudouche <boudouche.hamza.13.07.2000@gmail.com>
2024-07-06 21:31:27 +0200

    Fix ocamllsp and add trim_whitespaces

--- init.lua
+++ init.lua
@@ -737,7 +737,7 @@ require('lazy').setup({
         },
         sorbet = {},
         ocamllsp = {
-          cmd = { 'ocamllsp' },
+          cmd = { vim.env.OPAM_SWITCH_PREFIX .. '/bin/ocamllsp' },
           filetypes = { 'ocaml', 'ocaml.menhir', 'ocaml.interface', 'ocaml.ocamllex', 'reason', 'dune' },
           root_dir = require('lspconfig.util').root_pattern('*.opam', 'esy.json', 'package.json', '.git', 'dune-project', 'dune-workspace'),
         },
