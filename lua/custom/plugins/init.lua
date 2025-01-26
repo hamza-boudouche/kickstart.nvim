f789f5628a774acf4d1068805e1fb7b206360aaa
hamza-boudouche <boudouche.hamza.13.07.2000@gmail.com>
2025-01-26 06:19:10 +0100

    Add nvim-ufo to manage folds

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -103,6 +103,11 @@ end
 
 vim.keymap.set('n', '<leader>ca', AddMarkdownCheckbox, { desc = 'add markdown checkbox' })
 
+vim.o.foldcolumn = '1' -- '0' is not bad
+vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+vim.o.foldlevelstart = 99
+vim.o.foldenable = true
+
 return {
   {
     'stevearc/oil.nvim',
@@ -369,6 +374,21 @@ return {
         code = {
           sign = false,
         },
+      }
+    end,
+  },
+  {
+    'kevinhwang91/nvim-ufo',
+    dependencies = { 'kevinhwang91/promise-async' },
+    config = function()
+      -- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
+      vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
+      vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
+
+      require('ufo').setup {
+        provider_selector = function(bufnr, filetype, buftype)
+          return { 'treesitter', 'indent' }
+        end,
       }
     end,
   },
